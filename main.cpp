72e925c1c03c569e7abc62428cb96b0285bc3c02
Peter Tse <tsehonam@gmail.com>
2017-12-30 00:16:30 +0800

    Updated main.cpp doc

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]) {
 	const std::string filename = (argc == 1) ? tempf : argv[1];
 
 	int w, h; // width and height of image
-	std::vector<uint8_t> img = readBMP(filename, w, h);
+	std::vector<uint8_t> img = readBMP(filename, w, h); // use getPixel() defined in helper.h to get pixel information
 	int cornerc = 0; // corner count
 	std::vector< std::pair<int, int> > cornerv{}; // vector of corner coordinates, stored x-coord to pair.first and y-coord to pair.second
 
