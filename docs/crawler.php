3135a7974154725d2ca02e601f30a9c649f8a4fe
dipsywong98 <facebookarmyoffcal@gmail.com>
2018-01-02 12:25:29 +0800

    add submitted file viewing

---
+++ docs/crawler.php
@@ -0,0 +1,14 @@
+<?php
+if(isset($_GET["url"])){
+  $url = base64_decode($_GET["url"]);
+  echo "<pre>";
+  echo test_input(file_get_contents($url));
+  echo "</pre>";
+}
+
+function test_input($data) {
+  // $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
